e0153aedd0835d0af1913f5e0ad6acd988b63ba0
xatier <xatierlike@gmail.com>
2013-10-28 01:55:28 +0800

    add zero extension for ori instruction

--- Sign_Extend.v
+++ Sign_Extend.v
@@ -11,11 +11,13 @@
 
 module Sign_Extend(
     data_i,
-    data_o
+    data_o,
+    select_i
     );
 
 //I/O ports
 input   [16-1:0] data_i;
+input            select_i;
 output  [32-1:0] data_o;
 
 //Internal Signals
@@ -26,11 +28,10 @@ reg     [32-1:0] data_o;
 
 always@(*) begin
 // Ref. http://excamera.com/sphinx/fpga-verilog-sign.html
-    // XXX: from ALU control,
-    // i need to have a wire (one bit) to determine if the operation is ori,
-    // if so, just insert 16'b zeros into data_i
 
-    data_o <= {16{data_i[15]}, data_i};
+    // in ori, the immediate part needs zero extension
+    data_o <= select_i ? {16{data_i[15]}, data_i} :
+                         {16{1'b0}, data_i};
 end
 
 endmodule
