24894b11a5162021ca07ae78421732866379c967
xatier <xatierlike@gmail.com>
2013-10-28 00:05:00 +0800

    white spaces issues

--- Sign_Extend.v
+++ Sign_Extend.v
@@ -2,18 +2,18 @@
 //--------------------------------------------------------------------------------
 //Version:     1
 //--------------------------------------------------------------------------------
-//Writer:      
+//Writer:
 //----------------------------------------------
-//Date:        
+//Date:
 //----------------------------------------------
-//Description: 
+//Description:
 //--------------------------------------------------------------------------------
 
 module Sign_Extend(
     data_i,
     data_o
     );
-               
+
 //I/O ports
 input   [16-1:0] data_i;
 output  [32-1:0] data_o;
@@ -22,12 +22,11 @@ output  [32-1:0] data_o;
 reg     [32-1:0] data_o;
 
 //Sign extended
-//
+
 
 always@(*) begin
 // Ref. http://excamera.com/sphinx/fpga-verilog-sign.html
     data_o <= {16{data_i[15]}, data_i};
 end
-          
-endmodule      
-     
+
+endmodule
--- Simple_Single_CPU.v
+++ Simple_Single_CPU.v
@@ -2,17 +2,17 @@
 //--------------------------------------------------------------------------------
 //Version:     1
 //--------------------------------------------------------------------------------
-//Writer:      
+//Writer:
 //----------------------------------------------
-//Date:        
+//Date:
 //----------------------------------------------
-//Description: 
+//Description:
 //--------------------------------------------------------------------------------
 module Simple_Single_CPU(
         clk_i,
-		rst_n
-		);
-		
+        rst_n
+        );
+
 //I/O port
 input         clk_i;
 input         rst_n;
@@ -22,57 +22,57 @@ input         rst_n;
 
 //Greate componentes
 ProgramCounter PC(
-        .clk_i(),      
-	    .rst_n (),     
-	    .pc_in_i() ,   
-	    .pc_out_o() 
-	    );
-	
+    .clk_i(),
+    .rst_n (),
+    .pc_in_i() ,
+    .pc_out_o()
+    );
+
 Adder Adder1(
-        .src1_i(),     
-	    .src2_i(),     
-	    .sum_o()    
-	    );
-	
+        .src1_i(),
+        .src2_i(),
+        .sum_o()
+        );
+
 Instr_Memory IM(
-        .pc_addr_i(),  
-	    .instr_o()    
-	    );
+        .pc_addr_i(),
+        .instr_o()
+        );
 
 MUX_2to1 #(.size(5)) Mux_Write_Reg(
         .data0_i(),
         .data1_i(),
         .select_i(),
         .data_o()
-        );	
-		
-Reg_File RF(
-        .clk_i(),      
-	    .rst_n() ,     
-        .RSaddr_i() ,  
-        .RTaddr_i() ,  
-        .RDaddr_i() ,  
-        .RDdata_i()  , 
-        .RegWrite_i (),
-        .RSdata_o() ,  
-        .RTdata_o()   
         );
-	
+
+Reg_File RF(
+        .clk_i(),
+        .rst_n() ,
+        .RSaddr_i() ,
+        .RTaddr_i() ,
+        .RDaddr_i() ,
+        .RDdata_i()  ,
+        .RegWrite_i (),
+        .RSdata_o() ,
+        .RTdata_o()
+        );
+
 Decoder Decoder(
-        .instr_op_i(), 
-	    .RegWrite_o(), 
-	    .ALU_op_o(),   
-	    .ALUSrc_o(),   
-	    .RegDst_o(),   
-		.Branch_o()   
-	    );
+        .instr_op_i(),
+        .RegWrite_o(),
+        .ALU_op_o(),
+        .ALUSrc_o(),
+        .RegDst_o(),
+        .Branch_o()
+        );
 
 ALU_Ctrl AC(
-        .funct_i(),   
-        .ALUOp_i(),   
-        .ALUCtrl_o() 
+        .funct_i(),
+        .ALUOp_i(),
+        .ALUCtrl_o()
         );
-	
+
 Sign_Extend SE(
         .data_i(),
         .data_o()
@@ -83,37 +83,35 @@ MUX_2to1 #(.size(32)) Mux_ALUSrc(
         .data1_i(),
         .select_i(),
         .data_o()
-        );	
-		
+        );
+
 alu ALU(
-		.rst_n(),
+        .rst_n(),
         .src1(),
-	    .src2(),
-	    .ALU_control(),
-	    .result(),
-		.zero(),
-		.cout(),
-		.overflow()
-	    );		
+        .src2(),
+        .ALU_control(),
+        .result(),
+        .zero(),
+        .cout(),
+        .overflow()
+        );
 Adder Adder2(
-        .src1_i(),     
-	    .src2_i(),     
-	    .sum_o()      
-	    );
-		
+        .src1_i(),
+        .src2_i(),
+        .sum_o()
+        );
+
 Shift_Left_Two_32 Shifter(
         .data_i(),
         .data_o()
-        ); 		
-		
+        );
+
 MUX_2to1 #(.size(32)) Mux_PC_Source(
         .data0_i(),
         .data1_i(),
         .select_i(),
         .data_o()
-        );	
+        );
 
-endmodule
-		  
-
+endmodule 
 
